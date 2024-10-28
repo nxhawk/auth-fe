da59ee7bc1e2fb2d95aa7852ce547a556381fa0f
21120447 <haonhat2729@gmail.com>
2024-10-28 23:18:26 +0700

    chore: update api

--- src/api/apiUser.ts
+++ src/api/apiUser.ts
@@ -2,12 +2,12 @@ import { ILoginUserReq, IRegisterUserReq } from "../types/user";
 import AxiosClient from "./axios";
 
 export const login = async (data: ILoginUserReq) => {
-  const res = await AxiosClient.post("/auth/login", data);
+  const res = await AxiosClient.post("/user/login", data);
   return res.data;
 };
 
 export const register = async (data: IRegisterUserReq) => {
-  const res = await AxiosClient.post("/auth/register", data);
+  const res = await AxiosClient.post("/user/register", data);
   return res.data;
 };
 
@@ -17,7 +17,7 @@ export const profile = async () => {
 };
 
 export const refreshToken = async (refreshToken: string) => {
-  const res = await AxiosClient.get("/auth/refresh", {
+  const res = await AxiosClient.get("/user/refresh", {
     headers: {
       Authorization: `Bearer ${refreshToken}`,
     },
